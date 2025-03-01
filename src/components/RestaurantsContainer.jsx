a64929272e7f17525872b07d53d3806e48b08192
alial <fofafaloo@yahoo.com>
2025-03-01 00:04:31 -0800

    update

--- src/components/RestaurantsContainer.jsx
+++ src/components/RestaurantsContainer.jsx
@@ -1,17 +1,17 @@
-import { restaurants } from "../data/data.js";
+// import { restaurants } from "../data/data.js";
 
-import Restaurant from "./Restaurant";
+// import Restaurant from "./Restaurant";
 
-function RestaurantsContainer(props) {
-  return (
-    <div className="restaurantContainer">
-      {restaurants.map(restaurant => <Restaurant />)}
+// function RestaurantsContainer(props) {
+//   return (
+//     <div className="restaurantContainer">
+//       {restaurants.map(restaurant => <Restaurant />)}
   
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default RestaurantsContainer;
+// export default RestaurantsContainer;
 
 
